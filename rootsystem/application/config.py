3d53376e4d6ce36815ca68b0f76ee4f0af22bb44
marcoslealsierra <marcoslealsierra90@gmail.com>
2019-01-11 11:42:09 +0100

    Add config VARS

--- rootsystem/application/config.py
+++ rootsystem/application/config.py
@@ -2,5 +2,13 @@
 # -*- coding: utf-8 -*-
 
 
-DEFAULT_RESOURCE = "/modulo/recurso"  # Módulo y recurso por defecto
+DEFAULT_RESOURCE = "/producto/agregar"  # Módulo y recurso por defecto
 SHOW_ERROR_404 = False
+STATIC_PATH = "/home/mleal/src/proyectos/marcosCRMcgi/rootsystem/static"
+TEMPLATE_PATH = "{}/template.html".format(STATIC_PATH)
+
+DB_HOST = "localhost" 
+DB_USER = "root"
+DB_PASS = "mysqlroot"
+DB_NAME = "marcoscrmcgi"
+db_data = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
