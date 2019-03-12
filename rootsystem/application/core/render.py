14e1c885ccd4f5d0b87ae552e8875857d8a6edae
marcoslealsierra <marcoslealsierra90@gmail.com>
2019-03-12 21:24:12 +0100

    Update dict with author config

--- rootsystem/application/core/render.py
+++ rootsystem/application/core/render.py
@@ -2,6 +2,8 @@
 from re import search
 from string import Template as PyTemplate
 
+from config import author_data
+
 
 class Template:
 
@@ -14,10 +16,12 @@ class Template:
             return t.read()
 
     def render(self, values):
+        values.update(author_data)
         return PyTemplate(self.template).safe_substitute(values) 
 
     def render_inner(self, inner):
         values = dict(content=inner)
+        values.update(author_data)
         render = PyTemplate(self.template).safe_substitute(values)
         return render
 
