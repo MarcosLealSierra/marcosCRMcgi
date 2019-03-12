ee5cf98d0ef825063b77828cf5239c10498ffc55
Eugenia Bahit <eugenia@bella.homelab.eugeniabahit.com>
2019-03-12 17:10:37 -0300

    Refactoring PEP8

--- rootsystem/application/core/stdobject.py
+++ rootsystem/application/core/stdobject.py
@@ -5,7 +5,10 @@ from settings import db_data
 class StdObject(object):
 
     def delete(self):
-        clase = self.__class__.__name__
-        sql = "DELETE FROM {c} WHERE {c}_id = {pi}".format(c = clase.lower(), 
-            pi = self.__dict__['{}_id'.format(clase.lower())])
+        clase = self.__class__.__name__.lower()
+        propiedad_id = '{}_id'.format(clase)
+
+        sql = "DELETE FROM {c} WHERE {c}_id = {pi}".format(c=clase, 
+            pi=self.__dict__[propiedad_id])
         DBQuery(db_data).execute(sql)
+
