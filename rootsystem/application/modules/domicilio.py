65defdf666d37bbc06f9d77cc825df77aef40714
marcoslealsierra <marcoslealsierra90@gmail.com>
2019-05-14 09:34:14 +0200

    Take out select definition and minor changes

--- rootsystem/application/modules/domicilio.py
+++ rootsystem/application/modules/domicilio.py
@@ -18,29 +18,27 @@ class Domicilio(StdObject):
             INSERT INTO     domicilio
                             (calle, numero, planta, puerta, ciudad)
             VALUES          ('{}', '{}', {}, '{}', '{}')
-        """.format(self.calle, self.numero, self.planta, self.puerta, 
-            self.ciudad)
+        """.format(
+            self.calle, 
+            self.numero, 
+            self.planta, 
+            self.puerta, 
+            self.ciudad
+        )
         self.domicilio_id = DBQuery(db_data).execute(sql)
-  
-    def select(self):
-        sql = """
-            SELECT      calle, numero, planta, puerta, ciudad
-            FROM        domicilio
-            WHERE       domicilio_id = {}
-        """.format(self.domicilio_id)
-        resultados = DBQuery(db_data).execute(sql)[0]
-        self.calle = resultados[0]
-        self.numero = resultados[1]
-        self.planta = resultados[2]
-        self.puerta = resultados[3]
-        self.ciudad = resultados[4]
-    
+     
     def update(self):
         sql = """
             UPDATE      domicilio
             SET         calle = '{}', numero = '{}', planta = {}, 
                         puerta = '{}', ciudad = '{}'
             WHERE       domicilio_id = {}
-        """.format(self.calle, self.numero, self.planta, self.puerta,
-            self.ciudad)
+        """.format(
+            self.calle, 
+            self.numero, 
+            self.planta, 
+            self.puerta, 
+            self.ciudad,
+            self.domicilio_id
+        )
         DBQuery(db_data).execute(sql)
